93b45082ab2a4cafab1da4247ffa72b38a987d05
Paul Turner <programming.hero@gmail.com>
2017-10-19 07:36:25 +0100

    Added initial version

---
+++ src/SpecFlow.DotNetCoreFix/CreateSpecFlowAppConfigTask.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Xml;
+
+namespace SpecFlow.DotNetCoreFix
+{
+    public class CreateSpecFlowAppConfigTask : Task
+    {
+        [Required]
+        public ITaskItem OutputFile { get; set; }
+
+        [Required]
+        public string UnitTestProvider { get; set; }
+        
+        public override bool Execute()
+        {
+            var settings = new XmlWriterSettings
+            {
+                Indent = true
+            };
+
+            using (var w = XmlWriter.Create(OutputFile.ItemSpec, settings))
+            {
+                w.WriteStartElement("configuration");
+
+                w.WriteStartElement("configSections");
+
+                w.WriteStartElement("section");
+                w.WriteAttributeString("name", "specFlow");
+                w.WriteAttributeString("type", "TechTalk.SpecFlow.Configuration.ConfigurationSectionHandler, TechTalk.SpecFlow");
+                w.WriteEndElement();
+
+                w.WriteEndElement();
+
+                w.WriteStartElement("specFlow");
+
+                w.WriteStartElement("unitTestProvider");
+                w.WriteAttributeString("name", UnitTestProvider);
+                w.WriteEndElement();
+
+                w.WriteEndElement();
+
+                w.WriteEndElement();
+            }
+
+            return true;
+        }
+    }
+}
---
+++ src/SpecFlow.DotNetCoreFix/CreateSpecFlowProjectFileTask.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System.Xml;
+
+namespace SpecFlow.DotNetCoreFix
+{
+    public class CreateSpecFlowProjectFileTask : Task
+    {
+        [Required]
+        public ITaskItem OutputFile { get; set; }
+
+        [Required]
+        public ITaskItem[] FeatureFiles { get; set; }
+
+        public override bool Execute()
+        {
+            const string ns = "http://schemas.microsoft.com/developer/msbuild/2003";
+
+            var settings = new XmlWriterSettings
+            {
+                Indent = true
+            };
+
+            using (var w = XmlWriter.Create(OutputFile.ItemSpec, settings))
+            {
+                w.WriteStartElement("Project", ns);
+                w.WriteAttributeString("ToolsVersion", "14.0");
+                w.WriteAttributeString("DefaultTargets", "Build");
+
+                w.WriteStartElement("PropertyGroup", ns);
+                w.WriteStartElement("RootNamespace", ns);
+                w.WriteValue("SpecFlow.GeneratedTests");
+                w.WriteEndElement();
+                w.WriteStartElement("AssemblyName", ns);
+                w.WriteValue("SpecFlow.GeneratedTests");
+                w.WriteEndElement();
+                w.WriteEndElement();
+
+                w.WriteStartElement("ItemGroup", ns);
+                w.WriteStartElement("None", ns);
+                w.WriteAttributeString("Include", "app.config");
+                w.WriteEndElement();
+
+                foreach (var featureFile in FeatureFiles)
+                {
+                    w.WriteStartElement("None", ns);
+                    w.WriteAttributeString("Include", featureFile.ItemSpec);
+
+                    w.WriteStartElement("Generator", ns);
+                    w.WriteValue("SpecFlowSingleFileGenerator");
+                    w.WriteEndElement();
+
+                    w.WriteStartElement("LastGenOutput", ns);
+                    w.WriteValue(featureFile.ItemSpec + ".cs");
+                    w.WriteEndElement();
+
+                    w.WriteEndElement();
+                }
+
+                w.WriteEndElement();
+
+                w.WriteEndElement();
+            }
+
+            return true;
+        }
+    }
+}
