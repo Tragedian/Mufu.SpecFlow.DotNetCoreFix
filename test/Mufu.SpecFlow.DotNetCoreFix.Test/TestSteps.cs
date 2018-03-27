36daf291b7584d876232f041032cf51b6fe0c8cc
Paul Turner <programming.hero@gmail.com>
2018-03-27 18:18:32 +0100

    Fix bugs in handling of NuGet paths and paths with whitespaces

--- test/Mufu.SpecFlow.DotNetCoreFix.Test/TestSteps.cs
+++ test/Mufu.SpecFlow.DotNetCoreFix.Test/TestSteps.cs
@@ -8,19 +8,16 @@ namespace Mufu.SpecFlow.DotNetCoreFix.Test
         [Given(@"I have entered (.*) into the calculator")]
         public void GivenIHaveEnteredIntoTheCalculator(int p0)
         {
-            throw new PendingStepException();
         }
 
         [When(@"I press add")]
         public void WhenIPressAdd()
         {
-            throw new PendingStepException();
         }
 
         [Then(@"the result should be (.*) on the screen")]
         public void ThenTheResultShouldBeOnTheScreen(int p0)
         {
-            throw new PendingStepException();
         }
     }
 }
